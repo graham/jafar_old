1a4ac472156b72f74a6c06b705762b5ec3e97251
Graham Abbott <graham.abbott@gmail.com>
2012-04-17 19:04:45 -0700

    setup builder

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from setuptools import setup, find_packages, Extension, Feature
+
+setup(
+        name="jafar",
+        version=0.1,
+        description="Jafar API framework.",
+        long_description="Jafar API Framework.",
+        author="Graham Abbott",
+        author_email="graham.abbott@gmail.com",
+        packages=find_packages(),
+        platforms=['any'],
+        zip_safe=True,
+    )
+
